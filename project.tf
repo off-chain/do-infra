b40ca431c8ef6781623c56f4ad64b5c4601dcf87
Leonard Mocanu <leonard.mocanu@ymail.com>
2022-12-13 17:20:24 +0200

    adds a project

---
+++ project.tf
@@ -0,0 +1,8 @@
+resource "digitalocean_project" "main" {
+  name        = format("%s-prod", var.project)
+  description = format("%s-prod apps", var.project)
+  purpose     = format("%s-prod apps", var.project)
+  environment = "Production"
+  is_default  = true
+  resources   = [digitalocean_domain.main.urn, digitalocean_domain.taraxa_hype_prod.urn, digitalocean_domain.taraxa_hype_qa.urn, digitalocean_kubernetes_cluster.main.urn]
+}
